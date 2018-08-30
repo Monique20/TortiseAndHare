dbce6599e88db7132c2ab3629f4334d22d052403
Monique Govender <MoniqueG@SAHOMELOANS.COM>
2018-08-30 10:48:47 +0200

    completed kata in c#

---
+++ Race/Race/TortiseAndHareRace.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Race
+{
+    public class TortiseAndHareRace
+    {
+        public string StartRace(int speed1, int speed2, int distance, int sleep, int getup)
+        {
+            var fallAsleepTime = GetFallAsleepTime(speed1, sleep);
+            var wakeUpTime = GetWakeUpTime(speed2, distance, getup);
+            int totalSleepTime = GetTotalSleepTime(fallAsleepTime, wakeUpTime);
+
+            int tortoiseRunTime = GetTortiseRunTime(speed2, distance);
+            int hareRunTime = GetHareRunTime(speed1, distance, totalSleepTime);
+
+            if (TortiseRunTimeGreaterThanHareRunTime(tortoiseRunTime, hareRunTime))
+            {
+                return "The hare won the race. The hare is sleeping for " + totalSleepTime + " minutes.";
+            }
+            if (HareRunTimeGreaterThanTortiseRunTime(tortoiseRunTime, hareRunTime))
+            {
+                return "The tortoise won the race. The hare is sleeping for " + totalSleepTime + " minutes.";
+            }
+            return "The hare and the tortoise tied. The hare is sleeping for " + totalSleepTime + " minutes.";
+        }
+
+        private static bool HareRunTimeGreaterThanTortiseRunTime(int tortoiseRunTime, int hareRunTime)
+        {
+            return hareRunTime > tortoiseRunTime;
+        }
+
+        private static bool TortiseRunTimeGreaterThanHareRunTime(int tortoiseRanTime, int hareRanTime)
+        {
+            return tortoiseRanTime > hareRanTime;
+        }
+
+        private static int GetHareRunTime(int speed1, int distance, int totalSleepTime)
+        {
+            return (distance / speed1) + totalSleepTime;
+        }
+
+        private static int GetTortiseRunTime(int speed2, int distance)
+        {
+            return distance / speed2;
+        }
+
+        private static int GetTotalSleepTime(int fallAsleepTime, int wakeUpTime)
+        {
+            return wakeUpTime - fallAsleepTime;
+        }
+
+        private static int GetWakeUpTime(int speed2, int distance, int getup)
+        {
+            return (distance - getup) / speed2;
+        }
+
+        private static int GetFallAsleepTime(int speed1, int sleep)
+        {
+            return sleep / speed1;
+        }
+    }
+}
---
+++ Race/RaceTests/TortiseAndHareRaceTests.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using Race;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTests
+{
+    [TestFixture]
+    public class TortiseAndHareRaceTests
+    {
+        [Test]
+        public void Race_GivenSpeed1Of100Speed2Of10DistanceOf1000SleepOf500AndGetupOf40_ShouldReturnTheTortoiseWonTheRaceTheHareIsSleepingFor91Minutes()
+        {
+            //Arrange
+            var speed1 = 100;
+            var speed2 = 10;
+            var distance = 1000;
+            var sleep = 500;
+            var getup = 40;
+
+            var sut = new TortiseAndHareRace();
+            //Act
+            var actual = sut.StartRace(speed1, speed2, distance, sleep, getup);
+            //Assert
+            var expected = "The tortoise won the race. The hare is sleeping for 91 minutes.";
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void Race_GivenSpeed1Of100Speed2Of10DistanceOf1500SleepOf500AndGetupOf100_ShouldReturnTheHareAndTheTortoiseTiedTheHareIsSleepingFor135Minutes()
+        {
+            //Arrange
+            var speed1 = 100;
+            var speed2 = 10;
+            var distance = 1500;
+            var sleep = 500;
+            var getup = 100;
+
+            var sut = new TortiseAndHareRace();
+            //Act
+            var actual = sut.StartRace(speed1, speed2, distance, sleep, getup);
+            //Assert
+            var expected = "The hare and the tortoise tied. The hare is sleeping for 135 minutes.";
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void Race_GivenSpeed1Of100Speed2Of10DistanceOf1200SleepOf500AndGetupOf100_ShouldReturnTheHareWonTheRaceTheHareSleepingFor105Minutes()
+        {
+            //Arrange
+            var speed1 = 100;
+            var speed2 = 10;
+            var distance = 1200;
+            var sleep = 500;
+            var getup = 100;
+
+            var sut = new TortiseAndHareRace();
+            //Act
+            var actual = sut.StartRace(speed1, speed2, distance, sleep, getup);
+            //Assert
+            var expected = "The hare won the race. The hare is sleeping for 105 minutes.";
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
